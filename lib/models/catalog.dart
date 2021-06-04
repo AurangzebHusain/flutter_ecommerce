92f9f4b85b6a43d82b600561142883f4c6794af1
Aurangzeb Husain <aurangzebhusain786@gmail.com>
2021-06-05 01:52:41 +0530

    Added mapping||displaying data from json

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,16 +1,9 @@
 import 'package:flutter/cupertino.dart';
-class CatalogModel{
-static  final items=[
-  Item(
-      id: 1,
-      name: "iPhone 12 Pro",
-      description: "Apple iPhone 12th generation",
-      price: 999,
-      color: "#33505a",
-      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
-];
 
+class CatalogModel {
+  static  List<Item> items=[];
 }
+
 class Item {
   final int id;
   final String name;
@@ -26,5 +19,25 @@ class Item {
       required this.price,
       required this.color,
       required this.image});
-}
 
+  // Factory ka kaam
+  // pehle logic frr intitialize krna
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      id: map['id'],
+      name: map['name'],
+      description: map['description'],
+      price: map['price'],
+      color: map['color'],
+      image: map['image'],
+    );
+  }
+  toMap()=>{
+    "id":id,
+    "name":name,
+    "description":description,
+    "price":price,
+    "color":color,
+    "image":image
+  };
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,28 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_ecommerce/models/catalog.dart';
 import 'package:flutter_ecommerce/utils/routes.dart';
 import 'package:flutter_ecommerce/widgets/drawer.dart';
 import 'package:flutter_ecommerce/widgets/itemwidgets.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   Function darkModeHandler;
 
   HomePage(@required this.darkModeHandler);
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    loadData();
+  }
+  loadData() async{
+    await Future.delayed(Duration(seconds: 4));
+  var catalogJson=await rootBundle.loadString("assets/files/catalog.json");
+  var decodedData=jsonDecode(catalogJson);
+  var productsData=decodedData["products"];
+  CatalogModel.items=List.from(productsData).map<Item>((item) => Item.fromMap(item)).toList();
+  setState(() {
+
+  });
+  // print(productsData);
+  }
+  @override
   Widget build(BuildContext context) {
-    final dummyList=List.generate(50, (index) => CatalogModel.items[0]);
+    // final dummyList=List.generate(50, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
-      body: ListView.builder(
-        itemCount: dummyList.length,
-        itemBuilder: (context,index){
-          return ItemWidget(item:dummyList[index]);
-        },
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: CatalogModel.items!=null&&CatalogModel.items.isNotEmpty?ListView.builder(
+          itemCount: CatalogModel.items.length,
+          itemBuilder: (context,index){
+            return ItemWidget(item:CatalogModel.items[index]);
+          },
+        ):Center(child: CircularProgressIndicator(),),
       ),
-      drawer: MyDrawer(darkModeHandler),
+      drawer: MyDrawer(widget.darkModeHandler),
     );
   }
 }
